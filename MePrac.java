7ec3eeabbc550ff47260776e371daa238d013d63
mskayboss <43168402+mskayboss@users.noreply.github.com>
2018-09-11 10:06:03 +0100

    Add files via upload
    This project finds the sum and  average of two numbers

---
+++ MePrac.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newclass;
+
+import java.util.*;
+
+
+/**
+ *
+ * @author Kema
+ */
+public class MePrac {
+    public static void main(String [] arg){
+    int a;
+    //declare object of Scanner Class
+    Scanner buf = new Scanner(System.in);
+    System.out.println("Enter Value of a: ");
+    //nextInt() method of Scanner class
+    a = buf.nextInt();
+    
+    System.out.println("Value of a: " + a);
+    }
+    
+}
---
+++ NewClass.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newclass;
+import java.util.Scanner;
+/**t
+ *
+ * @author Kema
+ */
+public class NewClass {
+
+    
+   
+    
+    
+    
+   
+            
+    /*
+     i* @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    /* 
+//    int x = 20;
+    char y = 'D' ;
+    double z = 15.55332;
+    boolean a = false ;
+    double b = 0.004 ;
+        System.out.println (a);
+        System.out.println(x);
+        System.out.println (b);
+        System.out.println(x+z);
+        System.out.println((x-z)/b);
+        
+        System.out.println("Value of x: " + x);
+        System.out.println("Value of z: " + z);
+        System.out.println("Value of b: " + b);
+        System.out.println("Value of (x+z): " + (x+z));
+        System.out.println("Value of (x-z): " + (x-z));
+        */
+
+//*
+
+        
+        /*Scanner W = new Scanner(System.in);
+        
+        System.out.println("Enter the value of first number: ");
+        
+        Double y = W.nextDouble();
+        
+         System.out.println("Enter the value of second number: ");
+        
+        Double z = W.nextDouble();
+        
+           
+        Double Result = y + z;
+                
+        System.out.println("The result is : " + Result);
+        
+        */
+        
+        //Solving a quadratic equation using the almighty formula
+                
+        
+       
+    }
+    
+}
